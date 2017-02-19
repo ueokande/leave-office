6c05b97f3ba15afabaf9c0bb2c1ea866f05ee64b
Shin'ya Ueoka <ueokande@i-beam.org>
2017-02-19 13:55:39 +0900

    Clean with lint

--- renderer/main.jsx
+++ renderer/main.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Timetable from './timetable';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Timetable from "./timetable";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
 injectTapEventPlugin();
 
 export default class Main extends React.Component {
 
   componentDidMount() {
-    this.refs.timetable.setState({
+    this.timetable.setState({
       entries: [{
-      key: 1,
-      arrival_time: new Date('2017-02-18T21:25'),
-      departure_time: new Date('2017-02-18T21:36'),
-    }, {
-      key: 2,
-      arrival_time: new Date('2017-02-18T21:31'),
-      departure_time: new Date('2017-02-18T21:42'),
-    }, {
-      key: 3,
-      arrival_time: new Date('2017-02-18T21:37'),
-      departure_time: new Date('2017-02-18T21:48'),
-    }]});
+        key: 1,
+        arrivalTime: new Date("2017-02-18T21:25"),
+        departureTime: new Date("2017-02-18T21:36")
+      }, {
+        key: 2,
+        arrivalTime: new Date("2017-02-18T21:31"),
+        departureTime: new Date("2017-02-18T21:42")
+      }, {
+        key: 3,
+        arrivalTime: new Date("2017-02-18T21:37"),
+        departureTime: new Date("2017-02-18T21:48")
+      }]
+    });
   }
 
-  render () {
+  render() {
     return (
       <MuiThemeProvider>
-        <Timetable ref="timetable"/>
+        <Timetable ref={(c) => { this.timeline = c; }} />
       </MuiThemeProvider>
     );
   }
--- renderer/timetable.jsx
+++ renderer/timetable.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-
-
-import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
+import React from "react";
+import { List, ListItem } from "material-ui/List";
 
 export default class Timetable extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      entries: []
-    }
+    this.state = { entries: []};
   }
 
   render() {
     return (
       <List>
         {
-           this.state.entries.map((d) => {
-             let text = d.arrival_time.getHours() + ':'+ d.arrival_time.getMinutes() + ' - ' +
-                        d.departure_time.getHours() + ':'+ d.departure_time.getMinutes();
-             return (<ListItem key={d.key} primaryText={text} />)
+           this.state.entries.map((entry) => {
+             const text = entry.arrival_time.getHours() + ":" +
+                     entry.arrival_time.getMinutes() + " - " +
+                     entry.departure_time.getHours() + ":" +
+                     entry.departure_time.getMinutes();
+
+             return (
+               <ListItem key={entry.key} primaryText={text} />
+             );
            })
         }
       </List>
-    )
+    );
   }
 }
